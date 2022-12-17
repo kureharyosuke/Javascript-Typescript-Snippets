22ecb78781ae444cf7e70bbd070593dad39a66b7
kureharyosuke <ohhayo.com@gmail.com>
2022-12-17 16:27:03 +0900

    add: approximatelyEqual

--- Javascript/Javascript-Snippets.js
+++ Javascript/Javascript-Snippets.js
@@ -2,7 +2,7 @@
  *
  * @param {Array<number>} arr
  * @param {BooleanConstructor} fn
- * @returns boolean
+ * @returns {boolean}
  */
 const allJS = (arr, fn = Boolean) => arr.every(fn);
 
@@ -15,8 +15,33 @@ console.log("allJS", allJS([1, 2, 3])); //allJS true
 /**
  *
  * @param {Array<number>} arr
- * @returns
+ * @returns {boolean}
  */
 const allEqualJS = (arr) => arr.every((val) => val === arr[0]);
 console.log("allEqualJS", allEqualJS([1, 2, 3, 4, 5, 6])); // false
 console.log("allEqualJS", allEqualJS([1, 1, 1, 1, 1, 1])); // true
+
+/**
+ *
+ * @param {number} v1
+ * @param {number} v2
+ * @param {number} epsilon
+ * @returns {boolean}
+ */
+const approximatelyEqualJS = (v1, v2, epsilon = 0.001) =>
+  Math.abs(v1 - v2) < epsilon;
+
+console.log(
+  "approximatelyEqualJS",
+  approximatelyEqualJS(Math.PI / 2.0, 1.5708)
+); // true
+console.log(
+  "approximatelyEqualJS",
+  approximatelyEqualJS(Math.PI / 2.0, 1.5608)
+); // false
+
+console.log(
+  `🚀 ~ file: Javascript-Snippets.js:28 ~ Math.PI / 2.0,`,
+  Math.PI / 2.0
+);
+console.log(`🚀 ~ file: Javascript-Snippets.js:28 ~ Math.PI`, Math.PI);
--- Typescript/Typescript-Snippets.ts
+++ Typescript/Typescript-Snippets.ts
@@ -4,8 +4,10 @@
  * @param {BooleanConstructor} fn
  * @returns {boolean}
  */
-const allTS = (arr: number[], fn?: (x: number) => boolean | undefined) =>
-  arr.every(fn as BooleanConstructor);
+const allTS = (
+  arr: number[],
+  fn?: (x: number) => boolean | undefined
+): boolean => arr.every(fn as BooleanConstructor);
 
 console.log(
   "allTS",
@@ -18,6 +20,20 @@ console.log("allTS", allTS([1, 2, 3])); //allTS true
  * @param {Array<number>} arr
  * @returns {boolean}
  */
-const allEqualTS = (arr: Array<number>) => arr.every((val) => val === arr[0]);
+const allEqualTS = (arr: Array<number>): boolean =>
+  arr.every((val) => val === arr[0]);
 console.log("allEqualTS", allEqualTS([1, 2, 3, 4, 5, 6])); // false
 console.log("allEqualTS", allEqualTS([1, 1, 1, 1, 1, 1])); // true
+
+/**
+ *
+ * @param {number} v1
+ * @param {number} v2
+ * @param {number} epsilon
+ * @returns {boolean}
+ */
+const approximatelyEqualTS = (
+  v1: number,
+  v2: number,
+  epsilon: number = 0.001
+): boolean => Math.abs(v1 - v2) < epsilon;
